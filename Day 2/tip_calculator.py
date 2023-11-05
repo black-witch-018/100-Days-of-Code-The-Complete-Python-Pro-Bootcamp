0837562b41609e87ce8ab62df18010eb66ea2736
Jaagrit <jaagritmarya@gmail.com>
2023-11-05 17:38:06 +0530

    Day - 2 Project

--- Day 2/tip_calculator.py
+++ Day 2/tip_calculator.py
@@ -3,7 +3,7 @@ bill = float(input("What was the total bill? $"))
 tip_percent = int(input("What percentage tip would you like to give? 10, 12 or 15? "))
 persons = int(input("How many people to split the bill? "))
 
-total_tip = (tip_percent/100)*bill;
+total_tip = (tip_percent/100)*bill
 total_bill = bill + total_tip
 # Formatting the output to show 33.60 instead of 33.6
 bill_share = format(round(total_bill / persons, 2),".2f")
