b1bd09acc1de43017f735722b31fa54bd9f9782b
Jaagrit <jaagritmarya@gmail.com>
2023-11-05 12:29:41 +0530

    Exercise 2.3

--- Day 2/Exercises/coding_exercise_2_3.py
+++ Day 2/Exercises/coding_exercise_2_3.py
@@ -1,1 +1,12 @@
-print("Hello World")
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+max_age = 90
+age = int(age)
+years_remaining = max_age - age
+days_left = years_remaining * 365
+weeks_left = years_remaining * 52
+months_left = years_remaining * 12
+print(f"You have {days_left} days, {weeks_left} weeks, and {months_left} months left.")
