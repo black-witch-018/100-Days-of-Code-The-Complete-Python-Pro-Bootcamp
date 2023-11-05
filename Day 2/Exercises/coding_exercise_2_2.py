5393e26ad1f2c8efaa47f0eb4e0ab4ead59ed66f
Jaagrit <jaagritmarya@gmail.com>
2023-11-05 11:57:12 +0530

    Exercise 1.2

--- Day 2/Exercises/coding_exercise_2_2.py
+++ Day 2/Exercises/coding_exercise_2_2.py
@@ -1,1 +1,10 @@
-print("Hello World")
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+weight = float(weight)
+height = float(height)
+bmi = weight / (height * height)
+print(int(bmi))
