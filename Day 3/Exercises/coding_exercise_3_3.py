8c007e580dc03cf57a804a1dea2dce73c240ddf1
Jaagrit <jaagritmarya@gmail.com>
2023-11-08 02:13:00 +0530

    Exercise 3.3

--- Day 3/Exercises/coding_exercise_3_3.py
+++ Day 3/Exercises/coding_exercise_3_3.py
@@ -1,1 +1,11 @@
-print("Hello World")
+# 🚨 Don't change the code below 👇
+year = int(input("Which year do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+if year % 4 == 0 and year % 100 != 0:
+  print("Leap year.")
+elif year % 400 == 0:
+  print("Leap year.")
+else:
+  print("Not leap year.")
