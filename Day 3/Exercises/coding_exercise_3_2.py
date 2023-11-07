9a38a898967f19e41a8eda5ca5ebfd7fdbeb2e21
Jaagrit <jaagritmarya@gmail.com>
2023-11-08 02:29:52 +0530

    Exercise 3.2

--- Day 3/Exercises/coding_exercise_3_2.py
+++ Day 3/Exercises/coding_exercise_3_2.py
@@ -10,7 +10,7 @@ bmi = round(bmi)
 if bmi<18.5:
   print(f"Your BMI is {bmi}, you are underweight.")
 elif bmi>=18.5 and bmi<25:
-  print("Your BMI is {bmi}, you have a normal weight.")
+  print(f"Your BMI is {bmi}, you have a normal weight.")
 elif bmi>=25 and bmi<30:
   print(f"Your BMI is {bmi}, you are slightly overweight.")
 elif bmi>=30 and bmi<35:
