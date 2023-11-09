babf7c7c280f08e7cb51d804f2f5cf6600a28649
Jaagrit <jaagritmarya@gmail.com>
2023-11-09 18:37:39 +0530

    Exercise 4.1

--- Day 4/Exercises/coding_exercise_4_1.py
+++ Day 4/Exercises/coding_exercise_4_1.py
@@ -1,1 +1,11 @@
-print("Hello World")
+#Write your code below this line 👇
+#Hint: Remember to import the random module first. 🎲
+
+import random
+
+random_number = random.randint(0,1)
+
+if(random_number == 1):
+  print("Heads")
+else:
+  print("Tails")
