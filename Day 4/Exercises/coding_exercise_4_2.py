7337b5c588746e5b48934dc7da11da12d038083a
Jaagrit <jaagritmarya@gmail.com>
2023-11-14 20:33:42 +0530

    Exercise 4.2

--- Day 4/Exercises/coding_exercise_4_2.py
+++ Day 4/Exercises/coding_exercise_4_2.py
@@ -1,1 +1,12 @@
-print("Hello World")
+import random
+# 🚨 Don't change the code below 👇
+test_seed = int(input("Create a seed number: "))
+random.seed(test_seed)
+
+# Split string method
+namesAsCSV = input("Give me everybody's names, seperated by a comma. ")
+names = namesAsCSV.split(", ")
+# 🚨 Don't change the code above 👆
+
+random_index = random.randint(0, len(names) - 1)
+print(f"{names[random_index]} is going to buy the meal today!")
