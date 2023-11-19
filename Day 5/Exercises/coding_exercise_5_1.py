e29d305e321efb03c655e3e933ba11979bac6464
Jaagrit <jaagritmarya@gmail.com>
2023-11-19 16:01:48 +0530

    Exercise 5.1

--- Day 5/Exercises/coding_exercise_5_1.py
+++ Day 5/Exercises/coding_exercise_5_1.py
@@ -1,1 +1,18 @@
-print("Hello World")
+# 🚨 Don't change the code below 👇
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
+# 🚨 Don't change the code above 👆
+
+
+#Write your code below this row 👇
+no_of_students = 0
+sum_of_heights = 0
+
+for student in student_heights:
+  no_of_students += 1
+  sum_of_heights += student
+
+average_height = round(sum_of_heights/no_of_students)
+
+print(average_height)
