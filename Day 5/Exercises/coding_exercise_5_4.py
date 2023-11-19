4a499b02ab5814d03dceba9caac2607c7b60255b
Jaagrit <jaagritmarya@gmail.com>
2023-11-19 16:53:21 +0530

    Exercise 5.4 - My Logic

--- Day 5/Exercises/coding_exercise_5_4.py
+++ Day 5/Exercises/coding_exercise_5_4.py
@@ -1,1 +1,15 @@
-print("Hello World")
+
+# First Logic
+
+#Write your code below this row 👇
+flag = True
+for i in range(1,101):
+  flag = True
+  if i%3 == 0:
+    print("Fizz", end="")
+    flag = False
+  if i%5 == 0:
+    print("Buzz", end="")
+  elif flag:
+    print(i,end="")
+  print()
